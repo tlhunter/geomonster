c5369dcd6f452c8d6d67c4dd6916f9ebe42b6fdd
Thomas Hunter <tlhunter@gmail.com>
2013-02-23 11:57:26 -0500

    transmitting monster movements to all sockets

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ var io = require('socket.io').listen(server);
 var geomonster = require('geomonster');
 
 geomonster
+	.setSockets(io)
 	.initializeMonsterPopulation(1000)
 	.initializeMonsterMovement();
 
 server.listen(parseInt(process.argv[2], 10) || 80);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/public/index.html');
+	res.sendfile(__dirname + '/public/index.html');
 });
 
 app.get('/all-monsters', function (req, res) {
@@ -27,9 +28,9 @@ app.get('/monster-types', function (req, res) {
 
 app.use('/', express.static(__dirname + '/public'));
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+//io.sockets.on('connection', function (socket) {
+  //socket.emit('news', { hello: 'world' });
+  //socket.on('my other event', function (data) {
+    //console.log(data);
+  //});
+//});
