52cf392dc97396e9625bc1a354f606adb21a76f0
Thomas Hunter <tlhunter@gmail.com>
2013-03-09 13:58:15 -0500

    omg go

--- monster-database.go
+++ monster-database.go
@@ -9,21 +9,79 @@
 package main
 
 import "fmt"
+import "time"
+
+const (
+	MAX_MONSTERS = 100000 // Upper limit of monsters to have in the world
+	MONSTER_EXP_INCREASE = 1 // EXP Points
+	MONSTER_EXP_TIMING = 5 // Seconds
+	MONSTER_EXPIRATION = 60 * 60 * 12 // Seconds
+)
+
+type Player struct {
+	id int
+	lat float64
+	lon float64
+}
+
+type Monster struct {
+	id int
+	lat float64
+	long float64
+	mtype int
+	level int
+	exp int
+	persist bool
+	last_seen int // Time
+	created int // Time
+}
 
 func main() {
 	fmt.Println("Hello, 世界")
+	fmt.Println(int32(time.Now().Unix()))
 }
 
-// DATA: Players: ID(bigint), LAT(float), LON(float)
-// DATA: Monsters: ID(bigint), LAT(float), LON(float), TYPE(int), LEVEL(int), EXP(bigint), LAST_SEEN(datetime), PERSIST(bool), CREATED(datetime)
+// Information about a player has been updated
+func input_player() {
 
-// INPUT: Function to update player location
-// INPUT: Function to remove player
-// INPUT: Function to manually add a monster
+}
 
-// INTERVAL: Delete monsters when their LAST_SEEN > 12 hours AND !PERSIST
-// INTERVAL: Slowly increase monster EXP, level them up
-// INTERVAL: Continuously move monsters around
+// A player should be removed from our list
+func input_player_remove() {
 
-// OUTPUT: Monster Info (location changed, level up)
-// OUTPUT: Monster has been destroyed
+}
+
+// The server is manually adding a monster, possibly for an event or a company special
+func input_add_monster() {
+
+}
+
+// A monster should be removed from our list
+func input_remove_monster() {
+
+}
+
+// Iterate through each monster, deleting them if LAST_SEEN > 12 hours AND !persist
+func event_delete_old_monsters() {
+
+}
+
+// Iterate through each monster, incrementing their exp by one point, increasing level if applicable
+func event_monster_experience() {
+
+}
+
+// Iterate through each monster, moving them slightly
+func event_monster_movement() {
+
+}
+
+// Send the information about a monster to the other server, aka their position or level probably changed
+func output_monster_info(id int) {
+
+}
+
+// Send the information about a monster being removed to the other server
+func output_monster_remove(id int) {
+
+}
